c28de7ed1df3416aa47880ec87c6db03393c341a
Jeffrey McBeth <mcbeth@broggs.org>
2021-04-11 09:43:42 -0400

    Nikola auto commit.
    Nikola version: 8.1.3

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Palmyra Stake YM"  # (translatable)
-BLOG_TITLE = "Youth Conference"  # (translatable)
+BLOG_TITLE = "Palmyra Youth Conference"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://palmyraym.github.io/"
